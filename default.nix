f87cbb329764d6045627da16d5123b8d79749fa3
Casey Marshall <git@cmars.tech>
2021-11-06 11:32:01 -0500

    chore: improve Nix environment
    Build now depends on `make` and `golangci-lint`. Adding these
    dependencies to the Nix development environment.
    
    This only affects those using Nix for declarative development
    environments.

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     nativeBuildInputs = with pkgs.buildPackages; [
-      go envsubst
+      go envsubst gnumake golangci-lint
     ];
     shellHook = ''
       export GOPATH="$HOME/.cache/gopaths/$(sha256sum <<<$(pwd) | awk '{print $1}')"
